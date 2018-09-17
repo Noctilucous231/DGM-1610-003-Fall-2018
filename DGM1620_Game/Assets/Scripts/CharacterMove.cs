29c0152f0f959e2e0423cd06398ae2783c533fff
Jacob Lane <jacob@telane.com>
2018-09-17 17:15:23 -0600

    started a new scene added sprites
    made the sprite character boi move left and right

--- DGM1620_Game/Assets/Scripts/CharacterMove.cs
+++ DGM1620_Game/Assets/Scripts/CharacterMove.cs
@@ -18,9 +18,33 @@ private bool Grounded;
 	void Start () {
 		print("Hello World!");	
 	}
+
+
+	void FixedUpdate () {
+		Grounded = Physics2D.OverlapCircle(GroundCheck.position, GroundCheckRadius, WhatIsGround);
+	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		// This code makes the character jump
+		if(Input.GetKeyDown (KeyCode.Space)&& Grounded){
+			Jump();
+	}
+
+	//This code makes the character move from side to side using the arrow keys
+	if(Input.GetKey (KeyCode.D)){
+		GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+	}
+	if(Input.GetKey (KeyCode.A)){
+		GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+
+		}
+
+	}
+
+	public void Jump(){
+		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
 	}
 }
