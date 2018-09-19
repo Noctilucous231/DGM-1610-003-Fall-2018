c10b80d48618cb2cbb557db9efdcf50a03be5b45
Jacob Lane <jacob@telane.com>
2018-09-19 16:20:02 -0600

    did a script of var and func
    I didn't really get the functions, and I think I did the variables right.

---
+++ DGM1620_Game/Assets/Scripts/VarFunc.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VarFunk : MonoBehaviour {
+	 
+	 //variables
+	 public int MoveSpeed;
+	 public Transform GroundCheck;
+	 private float JumpHeight;
+	 //functions
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
