5a703fd6300459c5a18c713cf35dcfb0d9894174
Jacob Lane <jacob@telane.com>
2018-10-03 16:29:38 -0600

    did a pop quiz
    assessed what we know

---
+++ DGM1620_Game/Assets/Scripts/pop quiz on declaring int.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	//declare three variables
+	public int Variable1;
+	public int Variable2;
+	private int Total;
+
+	void AddVariables (){
+		Total = Variable1 + Variable2;
+			print(Total);
+	}
+
+
+
+
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
