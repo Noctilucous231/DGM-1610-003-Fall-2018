2a7d30ac022edd13f9f2075c8d31db8e43bb10c8
Jacob Lane <jacob@telane.com>
2018-10-08 16:20:33 -0600

    added new script
    German Shepard script

--- DGM1620_Game/Assets/Scripts/German Shepard.cs
+++ DGM1620_Game/Assets/Scripts/German Shepard.cs
@@ -2,12 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Canine : Animal {
-
-	public bool Tail;
-	public bool Bark;
-	public Color CoatColor;
-	public int Size;
+public class GermanShepard : Canine {
 
 	// Use this for initialization
 	void Start () {
