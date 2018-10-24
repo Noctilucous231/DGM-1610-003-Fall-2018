b8e6dc746ce0c160c195f8cd705c9eb225f98431
Jacob Lane <jacob@telane.com>
2018-10-24 17:17:35 -0600

    added some stuff for loops
    we made loops and had it count then we got homework.

---
+++ DGM1620_Game/Assets/Scripts/Loops.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour {
+
+	//	public int Num = 40;
+
+	//	public string Color = "Blue";
+
+	// Use this for initialization
+	//void Start () {
+
+		// 3 For Loops
+
+		// 3 While Loops
+
+
+
+
+
+		// for(int i=1; i <= 100; i++){
+
+			// print(i + "Ants Marching");
+
+		// }
+
+		//while(Num >= 0){
+		//	print("Countdown" + Num);
+		//	Num --;
+		// }
+
+	//	while( Color == "Red"){
+	//		Print("Color is" + Color);
+	//	}
+	//}
+	
+	// Update is called once per frame
+	//void Update () {
+		
+//	}
+}
