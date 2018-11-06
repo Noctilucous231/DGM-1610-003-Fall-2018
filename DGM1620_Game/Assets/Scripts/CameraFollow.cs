43bcde4d7e0c7f784e2092945ac7ec96312f7d53
Jacob Lane <jacob@telane.com>
2018-11-05 17:11:55 -0700

    did some things with loops and such
    we added some things to make a new camera and flipped the character

---
+++ DGM1620_Game/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public CharacterMove Boi;
+
+	public bool isFollowing;
+
+	//Camera position offset
+	public float xOffset;
+	public float yOffset;
+
+	// Use this for initialization
+	void Start () {
+		Boi = FindObjectOfType<CharacterMove>();
+
+		isFollowing = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isFollowing){
+			transform.position = new Vector3(Boi.transform.position.x + xOffset, Boi.transform.position.y + yOffset, transform.position.z);
+		}		
+	}
+}
---
+++ DGM1620_Game/Assets/Scripts/Homework/Arrays.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrays : MonoBehaviour {
+
+	//5 for each loops / for loops
+	//5 arrays
+
+	public int[] Eggs = new int[12];
+	public string[] Loot = new string[10];
+	public string[] Jedi = new string[12];
+
+	// Use this for initialization
+	void Start () {
+		
+		Loot = new string[10];
+		Loot[0] = "money";
+		Loot[1] = "potions";
+		Loot[2] = "gem";
+		Loot[3] = "weapon";
+		Loot[4] = "arrows";
+		Loot[5] = "bombs";
+		Loot[6] = "boots";
+		Loot[7] = "chest armor";
+		Loot[8] = "pant";
+		Loot[9] = "mega powerup";
+
+		foreach(string item in Loot){
+			print("Monster dropped " + item);
+		}
+
+		Eggs[0] = 1;
+		Eggs[1] = 2;
+		Eggs[2] = 3;
+		Eggs[3] = 4;
+		Eggs[4] = 5;
+		Eggs[5] = 6;
+		Eggs[6] = 7;
+		Eggs[7] = 8;
+		Eggs[8] = 9;
+		Eggs[9] = 10;
+		Eggs[10] = 11;
+		Eggs[11] = 12;
+
+		print(Eggs[8]);
+
+		Jedi = new string[5];
+
+		Jedi[0] = "Obi wan kenobi";
+		Jedi[1] = "Luke";
+		Jedi[2] = "Jar Jar";
+		Jedi[3] = "jawa";
+		Jedi[4] = "Anakin";
+
+		foreach(string item in Jedi){
+			print("jedi master " + item);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
